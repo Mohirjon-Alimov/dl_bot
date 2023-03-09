8f276285cc9f424ff139fe4b660741a9841995ba
Mohirjon <100334903+Mohirjon-Alimov@users.noreply.github.com>
2023-03-10 00:02:30 +0500

    add video-send func

--- bot.mjs
+++ bot.mjs
@@ -1,4 +1,8 @@
 import TeleBot from "telebot"
+import dotenv from "dotenv";
+import ttdl from "tiktok-video-downloader";
+let date = new Date();
+dotenv.config();
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
@@ -7,7 +11,8 @@ bot.on(/\/start/, (msg) => {
       msg?.chat?.id,
       "CAACAgQAAxkBAANWY2QEPlzq3vim0i9HELYo0hRrhwkAAhUDAAIv2CUOAnTLZ07ySQMqBA"
     );
-    bot.sendMessage(msg.chat.id, `Hello ${msg.from.first_name}`, );
+    bot.sendMessage(msg.chat.id, `Hello ${msg.from.first_name}` );
+    return
   });
 
 bot.on("text", async (msg) => {
@@ -24,7 +29,30 @@ bot.on("text", async (msg) => {
         return;
     }
     const url = msg.text;
-    bot.sendMessage(msg.from.id, `your url: ${url}`)
+    if (url.startsWith("https://vt.tiktok.com/")) {
+        try {
+          ttdl.getInfo(url).then(async (result) => {
+            await bot.sendVideo("-1001859543798", result.video.url.no_wm, {
+              caption: `link: ${url}\nname: ${msg.from.first_name}\nusername: @${
+                msg.from.username
+              }\n${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()} `,
+            });
+    
+            await bot.sendVideo(msg.from.id, result.video.url.no_wm, {
+              caption: "Downloaded with @tt_dl_free_hosting_bot",
+            });
+          });
+          return;
+        } catch (err) {
+          console.log(err);
+          msg.reply.text("Ошибка при загрузке видео.");
+          return;
+        }
+        return;
+      } else {
+        msg.reply.text("Это не ссылка на видео TikTok.");
+        return;
+      }
 });
 
 
