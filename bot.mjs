b59f71ca3cb839f26ac2eb758698fc2b249a4d6b
Mohirjon <100334903+Mohirjon-Alimov@users.noreply.github.com>
2023-03-09 23:29:55 +0500

    add start command

--- bot.mjs
+++ bot.mjs
@@ -3,5 +3,13 @@ import TeleBot from "telebot"
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 bot.on('text', msg => msg.reply.text(msg.text))
+bot.on("/start", (msg) => {
+    console.log(msg);
+    const chat_id = msg.from.id;
+    bot.sendMessage(
+        chat_id,
+        `Привет ${msg.from.first_name}! Я бот для скачивания видео из TikTok. Просто отправь мне ссылку на видео.`
+    );
+});
 
 export default bot
